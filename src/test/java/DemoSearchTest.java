04b05443e127f4c0bd612618e6e3623d8cd249de
Matas Backevicius <84940755+Mugdass@users.noreply.github.com>
2025-12-08 00:30:24 -0800

    Refactor DemoSearchTest to use JUnit 5

--- src/test/java/DemoSearchTest.java
+++ src/test/java/DemoSearchTest.java
@@ -1,51 +1,22 @@
-package demo;
-
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
-public class DemoSearchTest {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1280,720");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-    }
+public class DemoSearchTest extends BaseTest {
 
     @Test
-    public void testGoogleSearch() throws InterruptedException {
-        // Accept cookies if necessary
-        try {
-            WebElement acceptButton = driver.findElement(By.xpath("//button[contains(text(),'I agree')]"));
-            acceptButton.click();
-        } catch (Exception e) {}
+    public void searchTest() {
+        driver.get("https://www.google.com");
 
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium WebDriver");
-        searchBox.sendKeys(Keys.RETURN);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(By.name("q")));
 
-        Thread.sleep(3000); // wait for results
-        Assert.assertTrue(driver.getTitle().toLowerCase().contains("selenium webdriver"));
-    }
+        searchBox.sendKeys("Selenium");
+        searchBox.submit();
 
-    @AfterMethod
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        wait.until(ExpectedConditions.titleContains("Selenium"));
     }
 }
